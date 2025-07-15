c709f41728d744d282a2079e05ca3be851e350fc
Görkem Demirtaş <72773406+Trou95@users.noreply.github.com>
2025-07-15 22:53:49 +0300

    Delete backend/dist directory

--- backend/dist/auth/jwt.strategy.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-declare const JwtStrategy_base: any;
-export declare class JwtStrategy extends JwtStrategy_base {
-    private authService;
-    constructor(authService: AuthService);
-    validate(payload: any): Promise<import("../users/entities/user.entity").User>;
-}
-export {};
